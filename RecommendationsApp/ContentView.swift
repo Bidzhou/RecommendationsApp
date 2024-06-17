9afbd81ff824a6b75bb2e0ce244a48fabc6387a8
Bidzhou <89260226211@mail.ru>
2024-06-17 21:26:38 +0300

    code 1 added

---
+++ RecommendationsApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  RecommendationsApp
+//
+//  Created by Frederico del' Bidzho on 17.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+//code
+
+#Preview {
+    ContentView()
+}
---
+++ RecommendationsApp/RecommendationsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecommendationsAppApp.swift
+//  RecommendationsApp
+//
+//  Created by Frederico del' Bidzho on 17.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct RecommendationsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
